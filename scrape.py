ddf34a2c9677649bd231de0ddd47c746199935e7
Dilan <diporrasc@unal.edu.co>
2024-05-11 21:08:08 -0500

    excluir clases en busqueda, añadir busqueda de un(1) producto

--- scrape.py
+++ scrape.py
@@ -1,14 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 
+class ValueNotFoundByClass(Exception):
+    pass
 
-    
-def clear_text(text: str):
-    for i in range(len(text)):
-        if text[-i-1] == '\r':
-            return text[:-i-1][1:]
-    return text
-
+# Clase que recibe un link y lo convierte en un objeto BeautifulSoup para facilitar la extracción de datos
 class Page(BeautifulSoup):
     def __init__(self, link) -> None:
         self.html = requests.get(link).text
@@ -18,11 +14,18 @@ class Page(BeautifulSoup):
 
 # Recibe un tag que continene la información de un producto para crear un objeto con los datos del procuto y facilitar su acceso
 class ProductCard():
-    def __init__(self, tag: BeautifulSoup, class_name, class_price, class_link) -> None:
+    def __init__(self, tag: BeautifulSoup, class_name:list=None, class_price:list=None, class_link:list=None, exc_class_name:list=None, exc_class_price:list=None, exc_class_link:list=None) -> None:
         self.tag = tag
         self._class_name = class_name
         self._class_price = class_price
         self._class_link = class_link
+        self._exc_class_name = exc_class_name
+        self._exc_class_price = exc_class_price
+        self._exc_class_link = exc_class_link
+        self.name = ""
+        self.price_txt = ""
+        self.price = -1
+        self.link = ""
         self.define_product()
     
     def define_product(self):
@@ -31,29 +34,85 @@ class ProductCard():
         self._compute_link()
 
     def _compute_name(self):
-        try:
-            self.name = self.tag.find(class_=self._class_name).text
+        if self._class_name == None:
+            self.name = "Sin nombre"
             return self.name
-        except AttributeError:
-            return None
+        
+        try:
+            #Se busca el tag de siguiendo el orden de las clases especificadas
+            section:list[BeautifulSoup] = [self.tag]
+            for i in range(len(self._class_name)):
+                section = self._search_class_in_list(section, self._class_name[i], self._exc_class_name)
+
+            if len(section) > 1:
+                raise ValueNotFoundByClass("Se encontraron varios nombres")
+            
+            self.name = section[0].text
+            return self.name
+        except Exception as e:
+            print(e, "No se pudo obtener el nombre")
+            return ""
     
     def _compute_price(self):
-        try:
-            self.price_txt = self.tag.find(class_=self._class_price).text
-            self._decode_price()
+        if self._class_price == None:
+            self.price = "Sin precio"
             return self.price
-        except AttributeError:
-            print("No se encontró el precio")
-            return None
+
+        try:
+            #Se busca el tag de siguiendo el orden de las clases especificadas
+            section:list[BeautifulSoup] = [self.tag]
+            for i in range(len(self._class_price)):
+                section = self._search_class_in_list(section, self._class_price[i], self._exc_class_price)
+
+            if len(section) > 1:
+                raise ValueNotFoundByClass("Se encontraron varios precios")
+            
+            self.price_txt = section[0].text
+            self.__decode_price()
+            return self.price
+        except Exception as e:
+            print(e, "No se pudo obtener el precio")
+            return -1
 
     def _compute_link(self):
-        try:
-            self.link = self.tag.find(class_=self._class_link).get("href")
+        if self._class_link == None:
+            self.link = "Sin link"
             return self.link
-        except AttributeError:
-            return None
+        
+        try:
+            #Se busca el tag de siguiendo el orden de las clases especificadas
+            section:list[BeautifulSoup] = [self.tag]
+            for i in range(len(self._class_link)):
+                section = self._search_class_in_list(section, self._class_link[i], self._exc_class_link)
+
+            if len(section) > 1:
+                print(section)
+                raise ValueNotFoundByClass("Se encontraron varios links")
+            
+            self.link = section[0].text
+            return self.name
+        except Exception as e:
+            print(e, "No se pudo obtener el link")
+            return ""
+        
+
+    def _search_class_in_list(self, sections:list, class_, excluded:list=None):
+        if excluded == None:
+            excluded = []
+        # Obtener todos los tags con la clase especificada en cada section
+        result = []
+        for i in range(len(sections)):
+            result = result + sections[i].find_all(class_=class_)
+            
+        # Filtrar los tags que no contienen las clases excluidas
+        filtered = []
+        for tag in result:
+            if any(exc in tag['class'] for exc in excluded):
+                continue
+            filtered.append(tag)
+        return filtered
     
-    def _decode_price(self):
+    def __decode_price(self):
         self.price = int(self.price_txt.replace(".", ""))
         return self.price
     
@@ -79,11 +138,18 @@ class Products(Page):
         self.prices = []
         self.links = []
     
-    def search_product(self, link: str):
+    def _enter_webpage(self, link):
         self.link = link
-        super().__init__(self.link)
+        super().__init__(link)
+
+    def search_products(self, link: str):
+        self._enter_webpage(link)
         self._compute_products()
         self._compute_info()
+
+    def get_product_by_link(self, link: str):
+        self._enter_webpage(link)
+        return self._compute_one_product(link)
 
     def _compute_info(self):
         for product in self.products:
@@ -91,7 +157,9 @@ class Products(Page):
             self.names.append(product.name)
             self.links.append(product.link)
 
-    def _compute_products(self): # Cada página tiene una estructura diferente, por lo que se debe sobreescribir
+    def _compute_products(self, *args): # Cada página tiene una estructura diferente, por lo que se debe sobreescribir
+        pass
+    def _compute_one_product(self, *args): # Cada página tiene una estructura diferente, por lo que se debe sobreescribir
         pass
     
     def print_products(self):
@@ -119,11 +187,25 @@ class MercadoLibre(Products):
     def __init__(self) -> None:
         super().__init__()
         self.page_name = "MercadoLibre"
-        self.__CARD_DATA = ["ui-search-item__title", "andes-money-amount__fraction", "ui-search-link"]
+        self.__CARD_DATA = [["ui-search-item__title"], ["ui-search-price ui-search-price--size-medium", "andes-money-amount", "andes-money-amount__fraction"], ["ui-search-item__group__element ui-search-link__title-card ui-search-link"], [], ["ui-search-price__original-value"], []]
 
 
-    def search_product(self, product: str):
-        super().search_product(f"https://listado.mercadolibre.com.co/{product}")
+    def search_products(self, product: str):
+        super().search_products(f"https://listado.mercadolibre.com.co/{product}")
+
+    def get_product_by_link(self, link: str):
+        return super().get_product_by_link(link)
+    
+    def _compute_one_product(self, link):
+        section = self.find(class_="ui-pdp-container__col col-2 mr-32")
+        CLASS_NAME = ["ui-pdp-title"]
+        CLASS_PRICE = ["ui-pdp-price__main-container", "andes-money-amount--cents-superscript", "andes-money-amount__fraction"]
+        EXCLUDED_CLASS_PRICE = ["ui-pdp-price__original-value"]
+        product = ProductCard(section, CLASS_NAME, CLASS_PRICE, exc_class_price=EXCLUDED_CLASS_PRICE)
+        product.link = link
+        return product
+
+
 
     def _compute_products(self):
         try:
@@ -155,15 +237,15 @@ class MercadoLibre(Products):
 
 if __name__ == "__main__":
     page = MercadoLibre()
-
-    page.search_product("iphone 15")
+    
+    page.search_products("Computador")
     page.print_products()
-    print("Promedio: ", page.average_price())
 
-    maximo = page.products[0]
-    for product in page.products:
-        if product.price > maximo.price:
-            maximo = product
+    product = page.get_product_by_link("https://www.mercadolibre.com.co/all-in-one-hp-24-cb0003la-amd-ryzen-3-8-gb-256-gb-238/p/MCO32669085")
+    print(product.name)
+    print(product.price)
+    print(product.link)
 
-    print(maximo)
-    print(maximo.link)
+    
+
+    
