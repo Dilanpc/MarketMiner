46a59ea818ec59120cc70e5cf4a09ecb76860acf
ssanchez8 <159304421+ssanchez8@users.noreply.github.com>
2024-08-25 03:14:28 -0500

    Add files via upload

---
+++ Wikis.py
@@ -0,0 +1,46 @@
+import requests
+from bs4 import BeautifulSoup
+
+class WikiPage:
+    def __init__(self, url):
+        self.url = url
+        self.page = requests.get(url)
+        self.soup = BeautifulSoup(self.page.text, 'html.parser')
+    
+    def get_title(self):
+        return self.soup.title.text
+    
+    # Método para obtener los párrafos de la página 
+    def get_paragraph(self):
+        for paragraph in self.soup.select('p'):
+            print(paragraph.text)
+            
+class Wikipedia(WikiPage):
+    def __init__(self, url):
+        super().__init__(url)
+        self.url = url
+        self.page = requests.get(url)
+        self.soup = BeautifulSoup(self.page.text, 'html.parser')
+        
+    def find_keyword(self, keywords):
+        for paragraph in self.soup.select('p'):
+            # Comprobar si alguna de las palabras clave está en el texto del párrafo
+            for keyword in keywords:
+                if keyword in paragraph.text:
+                    print(paragraph.text)
+                    print("-------------------------------------------------")
+                    break 
+                
+                
+    
+wikipedia_page = Wikipedia('https://es.wikipedia.org/wiki/Lionel_Messi')
+
+
+
+print(wikipedia_page.get_title())
+
+# Definir la lista de palabras clave a buscar
+keywords = ["Balón de Oro", "Balones de Oro"]
+
+# Llamar al método para encontrar párrafos que contengan las palabras clave
+wikipedia_page.find_keyword(keywords)
