fce1e6a7c825e750a7ba61dac65e5bd5b89e95d3
ssanchez8 <159304421+ssanchez8@users.noreply.github.com>
2024-08-25 19:31:04 -0500

    Add files via upload

--- Wikis.py
+++ Wikis.py
@@ -31,14 +31,57 @@ class Wikipedia(WikiPage):
                     print("-------------------------------------------------")
                     break 
                 
+class WikiQuoteScience(WikiPage):
+    def __init__(self, url):
+        super().__init__(url)
+        self.url = url
+        self.page = requests.get(url)
+        self.soup = BeautifulSoup(self.page.text, 'html.parser')
+        
+    def find_quotes(self, quote):
+        quotes = []
+        for quote in self.soup.select('li'):
+            for keyword in keywords:
+                if keyword in quote.text:
+                    highlited_quote = quote.text.replace(keyword, f"\033[1;31;40m{keyword}\033[m")
+                    quotes.append(highlited_quote)
+                    break
+        
+        return quotes
+            
+    def find_author(self, author):
+        authors = []
+        for author in self.soup.select('li'):
+            author_tag = author.find('a', title=True)
+            if author_tag:
+                authors.append(author_tag.text)
+            else:
+                authors.append("Anónimo")
+        return authors
+            
+
                 
 if __name__ == "__main__":
+    # WIKIPEDIA
     wikipedia_page = Wikipedia('https://es.wikipedia.org/wiki/Lionel_Messi')
-
-    print(wikipedia_page.get_title())
-
-    # Definir la lista de palabras clave a buscar
+    
+ # Definir la lista de palabras clave a buscar
     keywords = ["Balón de Oro", "Balones de Oro"]
+    
+    print(wikipedia_page.get_title())  
 
     # Llamar al método para encontrar párrafos que contengan las palabras clave
     wikipedia_page.find_keyword(keywords)
+    
+    
+    # WIKIQUOTE
+    
+    wiki_quote_page = WikiQuoteScience('https://es.wikiquote.org/wiki/Ciencia')
+    keywords = ["ciencia", "científico"]
+    print(wiki_quote_page.get_title())    
+    quotes = wiki_quote_page.find_quotes(keywords)
+    authors = wiki_quote_page.find_author(keywords)
+    
+    for quote, author in zip(quotes, authors):
+        print(f"{quote} - {author}")
+        print("-------------------------------------------------")
