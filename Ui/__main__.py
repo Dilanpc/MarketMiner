eb985951413fbc27ed678aaa5697fd80f649e435
Dilan <diporrasc@unal.edu.co>
2024-09-12 21:27:55 -0500

    Corregir archivo __main__ de Ui

--- Ui/__main__.py
+++ Ui/__main__.py
@@ -1,16 +1,10 @@
-import sys
-
 from Ui.interface import Interface
-
-from PySide6.QtWidgets import QApplication
 
 
 
 def main():
-    app = QApplication(sys.argv)
-    window = Interface()
-    window.show()
-    app.exec()
+    app = Interface()
+    return app.exec()
 
 if __name__ == "__main__":
     main()
