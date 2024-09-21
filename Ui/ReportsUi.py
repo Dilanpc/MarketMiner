f14ccb003604dc93c44153c782190fac9df61716
Dilan <diporrasc@unal.edu.co>
2024-09-20 21:28:05 -0500

    Funcionalidad para hacer reportes

--- Ui/ReportsUi.py
+++ Ui/ReportsUi.py
@@ -2,9 +2,12 @@
 from PySide6.QtWidgets import (QWidget, QFrame, QVBoxLayout, QGridLayout, QScrollArea, QLabel,
 QPushButton, QLineEdit, QComboBox, QSizePolicy
 )
+from PySide6.QtCore import QThread, Signal
 
 from MarketMiner.reporter import ReportManager
 
+
+import time
 
 class ReportHeader(QWidget):
     def __init__(self, parent, reportsManager, reportList):
@@ -95,12 +98,12 @@ class ReportHeader(QWidget):
             "query": query,
             "product": None
         }
-        self.reportsManager.add(data)
+        self.reportsManager.add(data) # Añade el reporte al archivo json
         self.nameEntry.clear()
         self.queryEntry.clear()
 
         # Actualizar lista de reportes
-        self.reportList.addCard(data)
+        self.reportList.addCard(self.reportsManager.reports[-1]) # Añade el reporte a la lista de reportes
 
 
 
@@ -108,6 +111,11 @@ class ReportList(QScrollArea):
     def __init__(self, parent):
         super().__init__(parent)
 
+        self.setStyleSheet("""
+            border-radius: 8px;
+            background-color: #595960;
+            """)
+        self.setFrameShape(QFrame.StyledPanel)
         self.setWidgetResizable(True)
         
         self.widget = QWidget(self)
@@ -123,14 +131,14 @@ class ReportList(QScrollArea):
         self.layout.setSpacing(2)
         self.setWidget(self.widget)
     
-    def updateCards(self, data:list[dict]):
+    def updateCards(self, reports:list):
         self.clear()
-        for i in range(len(data)-1, -1, -1): # Recorrer del más reciente al más antiguo
-            self.cards.append(ReportCard(self.widget, data[i]))
+        for i in range(len(reports)-1, -1, -1): # Recorrer del más reciente al más antiguo
+            self.cards.append(ReportCard(self.widget, reports[i]))
             self.layout.addWidget(self.cards[-1])
     
-    def addCard(self, data:dict):
-        card = ReportCard(self.widget, data)
+    def addCard(self, report):
+        card = ReportCard(self.widget, report)
         self.cards.insert(0, card) # Agragar al principio
         self.layout.insertWidget(0, card)
 
@@ -146,7 +154,7 @@ class ReportList(QScrollArea):
 
 
 class ReportCard(QFrame):
-    def __init__(self, parent, data:dict):
+    def __init__(self, parent, report):
         super().__init__(parent)
 
         self.setSizePolicy(QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Maximum))
@@ -160,10 +168,10 @@ class ReportCard(QFrame):
         """
         )
 
-        self.data = data
+        self.report = report
 
         self.title = QLabel(self)
-        self.title.setText(data['name'])
+        self.title.setText(report['name'])
         self.title.setWordWrap(True)
         self.title.setStyleSheet("""
             color: white;
@@ -174,7 +182,7 @@ class ReportCard(QFrame):
         """)
 
         self.query = QLabel(self)
-        self.query.setText(data['query'])
+        self.query.setText(report['query'])
         self.query.setStyleSheet("""
             color: white;
             font-size: 14px;
@@ -183,7 +191,7 @@ class ReportCard(QFrame):
         """)
 
         self.class_ = QLabel(self)
-        self.class_.setText(data['class'])
+        self.class_.setText(report['class'])
         self.class_.setStyleSheet("""
             color: white;
             font-size: 14px;
@@ -192,13 +200,33 @@ class ReportCard(QFrame):
         """)
 
         self.path = QLabel(self)
-        self.path.setText(data['reportPath'])
+        self.path.setText(report['reportPath'])
         self.path.setStyleSheet("""
             color: white;
             font-size: 12px;
             background-color: #555;
             margin: 5px;
         """)
+
+
+        self.startbtn = QPushButton(self)
+        self.startbtn.setText("Iniciar")
+        self.startbtn.setStyleSheet("""
+            QPushButton {
+                background-color: #999;
+                color: Black;
+                border-radius: 8px;
+                padding: 5px;
+            }
+            QPushButton:hover {
+                background-color: #bbb;
+            }
+            QPushButton:pressed {
+                background-color: #888;
+            }
+            """)
+        self.startbtn.clicked.connect(self.run)
+
 
         self.layout = QGridLayout(self)
         self.layout.setSpacing(0)
@@ -207,6 +235,32 @@ class ReportCard(QFrame):
         self.layout.addWidget(self.class_, 0, 2, 1, 1)
         self.layout.addWidget(self.path, 1, 1, 1, 2)
 
+        self.layout.addWidget(self.startbtn, 0, 3, 2, 1)
+
+    def run(self):
+        self.startbtn.setEnabled(False)
+        self.reportThread = ReportThread(self.report)
+        self.reportThread.ready.connect(self.reportFinished)
+        self.reportThread.start()
+
+    def reportFinished(self, success):
+        self.startbtn.setEnabled(True)
+        self.reportThread.deleteLater()
+
+
+class ReportThread(QThread):
+    ready = Signal(bool)
+    def __init__(self, report):
+        super().__init__()
+        self.report = report
+
+    def run(self):
+        try:
+            self.report.run()
+            self.ready.emit(True)
+        except Exception as e:
+            print(f"Error en reporte de {self.report['name']}: {e}")
+            self.ready.emit(False)
 
 
 
@@ -220,7 +274,7 @@ class ReportsTab(QWidget):
         self.reportList = ReportList(self)
         self.header = ReportHeader(self, self.reportManager, self.reportList)
 
-        self.reportList.updateCards(self.reportManager.data)
+        self.reportList.updateCards(self.reportManager.reports)
 
 
         layout = QVBoxLayout(self)
